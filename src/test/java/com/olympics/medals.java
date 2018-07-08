523a714af38e6382aa2a59916bdbc185d9596682
kinziva <hannahkaplan08@gmail.com>
2018-07-08 01:59:10 -0400

    edited

--- src/test/java/com/olympics/medals.java
+++ src/test/java/com/olympics/medals.java
@@ -1,31 +1,23 @@
 package com.olympics;
 
 import java.util.Map.Entry;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -34,15 +26,15 @@ public class medals {
 	static WebDriver driver;
 	String url = "https://en.wikipedia.org/wiki/2016_Summer_Olympics#Medal_table.";
 
-	@BeforeClass // runs once for all tests
+	@BeforeClass 
 	public void setUp() {
 
 		WebDriverManager.chromedriver().setup();
 
 		driver = new ChromeDriver();
-		 driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-  	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		// driver.manage().window().fullscreen();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+     	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
 
 		driver.get("https://en.wikipedia.org/wiki/2016_Summer_Olympics#Medal_table");
 		WebElement point = driver
@@ -54,47 +46,34 @@ public class medals {
 
 	@Test(priority = 1)
 	public void sortTestCase1() {
-		List<WebElement> firstColumn = driver.findElements(
-				By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/td[1]"));
-
+		List<WebElement> firstColumn = driver.findElements(By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/td[1]"));
 		ArrayList<Integer> actualList = new ArrayList<>();
+		
 		for (int i = 0; i < firstColumn.size() - 1; i++) {
 			actualList.add(Integer.parseInt(firstColumn.get(i).getText()));
-
 		}
-		System.out.println(actualList);
-
 		SortedSet<Integer> expectedList = new TreeSet<>(actualList);
-		System.out.println(expectedList);
 		Assert.assertEquals(actualList, expectedList);
-
+		
 		driver.findElement(By.xpath("//th[@class='headerSort'][contains(text(),'NOC')]")).click();
-
 		//countries
-		List<WebElement> countries = driver.findElements(
-				By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/th/a"));
-
+		List<WebElement> countries = driver.findElements(By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/th/a"));
 		ArrayList<String> actualCountryList = new ArrayList<>();
 
 		for (int i = 0; i < countries.size() - 1; i++) {
 			actualCountryList.add(countries.get(i).getText());
-			// System.out.println(actualCountryList);
 		}
 		System.out.println(actualCountryList + " Arraylist");
 		SortedSet<String> expectedCountryList = new TreeSet<>(actualCountryList);
 		System.out.println(expectedCountryList + "Treeset");
 		Assert.assertEquals(actualCountryList, expectedCountryList);
-
-		List<WebElement> firstColumn1 = driver.findElements(
-				By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/td[1]"));
+		
+		List<WebElement> firstColumn1 = driver.findElements(By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/td[1]"));
 		ArrayList<Integer> actualList1 = new ArrayList<>();
 		for (int i = 0; i < firstColumn.size() - 1; i++) {
 			actualList1.add(Integer.parseInt((firstColumn1.get(i).getText())));
 		}
-		System.out.println("ex" + expectedList);
-		System.out.println("ac" + actualList1);
 		Assert.assertFalse(expectedList.equals(actualList1));
-
 	}
 
 	@Test(priority = 2)
@@ -194,28 +173,20 @@ public class medals {
 	    Assert.assertEquals(getSum2(), expectedtotal18);
 	}
 	
-	
 	public static List<String> getSum2() {
 		List<WebElement> bronze = driver.findElements(By.xpath("//table[@class = 'wikitable sortable plainrowheaders jquery-tablesorter']//tbody//td[4]"));
 		List<WebElement> countries = driver.findElements(By.xpath("//table[@class='wikitable sortable plainrowheaders jquery-tablesorter']/tbody/tr/th/a"));
-		 Map<String,Integer> bronzeTotal18 = new HashMap<>();
-		 for (int i = 0; i < countries.size(); i++) {
-			 bronzeTotal18.put((countries.get(i).getText()), Integer.parseInt(bronze.get(i).getText()));
-			 System.out.println("country: " + countries.get(i).getText()+ " bronze medal count: " +bronze.get(i).getText());
-			}
-//		 for (Map.Entry<String,Integer> entry : bronzeTotal18.entrySet()) {
-//			 
-//		 }
-		 
+//		Map<String,Integer> bronzeTotal18 = new HashMap<>();
+//		 for (int i = 0; i < countries.size(); i++) {
+//			 bronzeTotal18.put((countries.get(i).getText()), Integer.parseInt(bronze.get(i).getText()));
+//			 System.out.println("country: " + countries.get(i).getText()+ " bronze medal count: " +bronze.get(i).getText());
+//			}
 		 List<String> ls = new ArrayList<>();
 		 
 		 for (int i = 0; i < countries.size(); i++) {
 			 for (int j = i+1; j < countries.size(); j++) {
 				 int first = Integer.parseInt(bronze.get(i).getText());
 				 int next = Integer.parseInt(bronze.get(j).getText());
-//				 String firstcountry =countries.get(i).getText());
-//				 String nextcountry = countries.get(j).getText());
-				 
 				 if ((first+next)==18) {
 					 ls.add(countries.get(i).getText() +" + " +countries.get(j).getText());
 				   }
@@ -223,36 +194,5 @@ public class medals {
 			   }
 		 System.out.println(ls);
 		return ls;
-	}
-//	public static List<String> getSum() {
-//
-//		List<WebElement> medals = driver.findElements(
-//				By.xpath("//table[@class='wikitable sortableplainrowheaders jquery-tablesorter']/tbody/tr/td[4]"));
-//		List<WebElement> countries = driver.findElements(
-//				By.xpath("//table[@class='wikitable sortableplainrowheaders jquery-tablesorter']/tbody/tr/th/a"));
-//		HashMap<String, Integer> mp = new HashMap<>();
-//		HashMap<String, Integer> hmp = new HashMap<>();
-//
-//		SortedSet<String> st = new TreeSet<>();
-//		for (int i = 0; i < medals.size() - 1; i++) {
-//			mp.put(countries.get(i).getText(), Integer.parseInt(medals.get(i).getText()));
-//		}
-//		for (int i = 0; i < medals.size() - 1; i++) {
-//			hmp.put(countries.get(i).getText(), Integer.parseInt(medals.get(i).getText()));
-//		}
-//		for (Entry<String, Integer> each : mp.entrySet()) {
-//			for (Entry<String, Integer> other : hmp.entrySet()) {
-//
-//				if (!(each.getKey().equals(other.getKey())) && (each.getValue() + other.getValue()) == 18) {
-//					st.add(each.getKey());
-//					st.add(other.getKey());
-//				}
-//			}
-//		}
-//
-//		List<String> ls = new ArrayList<>(st);
-//
-//		return ls;
-//	}
-	
+	}	
 }
